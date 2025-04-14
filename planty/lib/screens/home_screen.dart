61797ec6154d185254d70aea52020668633cadf0
김민지 <ghi512151@gmail.com>
2025-04-15 03:35:26 +0900

    [test] #1 - 테스트용 로그아웃 기능 구현

--- planty/lib/screens/home_screen.dart
+++ planty/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:planty/screens/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +13,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('홈화면', style: TextStyle(fontSize: 24))),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('홈화면', style: TextStyle(fontSize: 24)),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                final storage = FlutterSecureStorage();
+                await storage.delete(key: 'token');
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const LoginScreen()),
+                );
+              },
+              child: const Text('로그아웃'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
