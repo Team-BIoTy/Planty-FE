c74e452b2fac63e38de8bd092c84cd8252c69650
김민지 <ghi512151@gmail.com>
2025-04-17 17:26:50 +0900

    [feat] #2 - 홈 상단 구조 구현 (하드코딩)

--- planty/lib/screens/home_screen.dart
+++ planty/lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:planty/constants/colors.dart';
 import 'package:planty/screens/login_screen.dart';
+import 'package:planty/widgets/user_plant_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,12 +15,76 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      backgroundColor: Colors.white,
+      body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('홈화면', style: TextStyle(fontSize: 24)),
+            // 나의 정원, 총 개수, 식물 등록 버튼
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        '나의 정원',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: AppColors.primary,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(width: 7),
+                      Text(
+                        '총 2개',
+                        style: TextStyle(
+                          fontSize: 9,
+                          color: AppColors.primary,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  // 식물 등록 버튼
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: AppColors.primary, width: 0.5),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.add_circle_rounded,
+                            color: AppColors.primary,
+                            size: 15,
+                          ),
+                          SizedBox(width: 3),
+                          Text(
+                            '식물 등록',
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: AppColors.primary,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             const SizedBox(height: 20),
+            // 식물 카드
+            UserPlantCard(),
+            // 로그아웃
             ElevatedButton(
               onPressed: () async {
                 final storage = FlutterSecureStorage();
