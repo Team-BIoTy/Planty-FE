7db69bfa788f8bb9ccfd58cb3be5b5c6a04bb819
김민지 <ghi512151@gmail.com>
2025-05-21 13:10:39 +0900

    [feat] #7 - Firebase FCM 연동 설정 (디바이스 토큰 수신 확인)

--- planty/android/app/build.gradle.kts
+++ planty/android/app/build.gradle.kts
@@ -3,16 +3,18 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.planty"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,4 +43,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+  implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
--- planty/lib/main.dart
+++ planty/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:planty/screens/chat/chat_list_screen.dart';
@@ -12,6 +14,10 @@ final RouteObserver<ModalRoute<void>> routeObserver =
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized;
   await dotenv.load();
+
+  await Firebase.initializeApp();
+  final token = await FirebaseMessaging.instance.getToken();
+  print('🔥 FCM Token: $token');
 
   runApp(const MyApp());
 }
